fbee2f64c20bbd6e5bd17d9194034adcedb4d647
Sujit Kaulavkar <sujit.kaulavkar@perennialsys.com>
2018-06-22 13:48:58 +0530

    Initial Commit

---
+++ SamplePod_SVGKit/SamplePod_SVGKit.h
@@ -0,0 +1,19 @@
+//
+//  SamplePod_SVGKit.h
+//  SamplePod_SVGKit
+//
+//  Created by Perennial  on 22/06/18.
+//  Copyright © 2018 Perennial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SamplePod_SVGKit.
+FOUNDATION_EXPORT double SamplePod_SVGKitVersionNumber;
+
+//! Project version string for SamplePod_SVGKit.
+FOUNDATION_EXPORT const unsigned char SamplePod_SVGKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamplePod_SVGKit/PublicHeader.h>
+
+
